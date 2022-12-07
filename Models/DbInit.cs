06756fe027f9d69b1038a809045697a679886a15
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-12-07 09:59:38 +0100

    Add: seed data for dbinit

---
+++ Models/DbInit.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Models.Enums;
+
+namespace HogwartsPotions.Models
+{
+    public class DbInit
+    {
+        public static void Init(HogwartsContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Students.Any())
+            {
+                return;
+            }
+
+            context.Students.AddRange(new Student[]
+            {
+                new Student("Harry", HouseType.Gryffindor, PetType.Owl),
+                new Student("Ron", HouseType.Gryffindor, PetType.Rat),
+                new Student("Luna", HouseType.Ravenclaw, PetType.None),
+                new Student("Draco", HouseType.Slytherin, PetType.None),
+                new Student("Hermione", HouseType.Gryffindor, PetType.Cat)
+            });
+            
+            context.SaveChanges();
+
+            context.Rooms.AddRange(new Room[]
+            {
+                new Room(4),
+                new Room(4),
+                new Room(4)
+            });
+
+            context.SaveChanges();
+
+            Student Harry = context.Students.First(s => s.Name == "Harry");
+            Student Ron= context.Students.First(s => s.Name == "Ron");
+            Student Luna= context.Students.First(s => s.Name == "Luna");
+            Student Draco= context.Students.First(s => s.Name == "Draco");
+            Student Hermione= context.Students.First(s => s.Name == "Hermione");
+
+            Room firstRoom = context.Rooms.First();
+            firstRoom.Residents.Add(Harry);
+            firstRoom.Residents.Add(Ron);
+            firstRoom.Residents.Add(Luna);
+            firstRoom.Residents.Add(Draco);
+
+            Room secondRoom = context.Rooms.First(r => r.CurrentCapacity > 0);
+            secondRoom.Residents.Add(Hermione);
+
+            context.SaveChanges();
+        }
+    }
+}
