3925c24f878c0020b0cec056b6f70e0f3980304c
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-12-08 11:37:35 +0100

    Fix: add list init to ingredients in model

--- Models/Entities/Potion.cs
+++ Models/Entities/Potion.cs
@@ -10,7 +10,7 @@ namespace HogwartsPotions.Models.Entities
         public long ID { get; set; }
         public string Name { get; set; }
         public Student Student { get; set; }
-        public List<Ingredient> Ingredients { get; set; }
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
         public BrewingStatus BrewingStatus { get; set; }
         public Recipe Recipe { get; set; }
 
