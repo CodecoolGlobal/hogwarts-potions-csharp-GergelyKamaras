c4355faebc58a30b62d25839bed25063d7e8b1df
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-12-08 12:01:22 +0100

    Add: EF migration file for db update

---
+++ Migrations/20221208105849_ModelUpdates.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HogwartsPotions.Migrations
+{
+    public partial class ModelUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfDiscoveries",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfPotions",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfDiscoveries",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfPotions",
+                table: "Students");
+        }
+    }
+}
